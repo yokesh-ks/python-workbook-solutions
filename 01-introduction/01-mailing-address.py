c620573b04f092a282dda350331b0cc17f95a8ba
YOKESH KS <ksyokesh98@gmail.com>
2026-03-18 19:33:17 +0530

    Add program to display mailing address

---
+++ 01-introduction/01-mailing-address.py
@@ -0,0 +1,5 @@
+# Create a program that displays your name and complete mailing address formatted in the manner that you would usually see it on the outside of an envelope. Your program does not need to read any input from the user.
+
+print("Yokesh K S")
+print("No 21, Bajanai Koil Street, Pungambedu")
+print("Minjur, Chennai - 601203")
